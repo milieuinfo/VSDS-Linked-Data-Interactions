b930f1a7c7c3f71fe70498f51e0866a8eb4b2f0c
sbylemans <sander.bylemans@continuum.be>
2024-06-05 16:29:02 +0200

    Fix build

--- ldi-nifi/ldi-nifi-processors/ldes-client-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/StatePersistenceFactory.java
+++ ldi-nifi/ldi-nifi-processors/ldes-client-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/StatePersistenceFactory.java
@@ -1,30 +1,33 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldi.processors;
 
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.processors.config.LdesProcessorProperties.getPostgresPassword;
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.processors.config.LdesProcessorProperties.getPostgresUrl;
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.processors.config.LdesProcessorProperties.getPostgresUsername;
 import static be.vlaanderen.informatievlaanderen.ldes.ldi.processors.config.LdesProcessorProperties.getStatePersistenceStrategy;
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.processors.config.LdesProcessorProperties.stateKept;
 
+import java.util.Map;
 import ldes.client.treenodesupplier.domain.valueobject.StatePersistence;
 import ldes.client.treenodesupplier.domain.valueobject.StatePersistenceStrategy;
 import ldes.client.treenodesupplier.repository.sql.postgres.PostgresProperties;
 import org.apache.nifi.processor.ProcessContext;
 
-import java.util.Map;
-
 public class StatePersistenceFactory {
 
-	public StatePersistence getStatePersistence(ProcessContext context) {
-		StatePersistenceStrategy state = getStatePersistenceStrategy(context);
-		Map<String, String> persistenceProperties = Map.of();
-		if (state.equals(StatePersistenceStrategy.POSTGRES)) {
-			persistenceProperties = createPostgresProperties(context);
-		}
-		return StatePersistence.from(state, persistenceProperties, context.getName());
-	}
+  public StatePersistence getStatePersistence(ProcessContext context) {
+    StatePersistenceStrategy state = getStatePersistenceStrategy(context);
+    Map<String, String> persistenceProperties = Map.of();
+    if (state.equals(StatePersistenceStrategy.POSTGRES)) {
+      persistenceProperties = createPostgresProperties(context);
+    }
+    return StatePersistence.from(state, persistenceProperties, context.getName());
+  }
 
-	private Map<String, String> createPostgresProperties(ProcessContext context) {
-		String url = LdesProcessorProperties.getPostgresUrl(context);
-		String username = LdesProcessorProperties.getPostgresUsername(context);
-		String password = LdesProcessorProperties.getPostgresPassword(context);
-		boolean keepState = LdesProcessorProperties.stateKept(context);
-		return new PostgresProperties(url, username, password, keepState).getProperties();
-	}
+  private Map<String, String> createPostgresProperties(ProcessContext context) {
+    String url = getPostgresUrl(context);
+    String username = getPostgresUsername(context);
+    String password = getPostgresPassword(context);
+    boolean keepState = stateKept(context);
+    return new PostgresProperties(url, username, password, keepState).getProperties();
+  }
 }
