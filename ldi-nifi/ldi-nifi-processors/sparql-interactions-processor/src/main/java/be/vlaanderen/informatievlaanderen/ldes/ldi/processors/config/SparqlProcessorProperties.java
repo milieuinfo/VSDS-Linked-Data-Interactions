d9cd4cd5674f516e15aa18c46accc4d3dd61748a
sbylemans <sander.bylemans@continuum.be>
2024-06-05 17:26:22 +0200

    Revert "Revert "support expressionlanguage on sparql select query property""
    This reverts commit 2cfaa0af93146aa308511ffc33fb61b3e0beb529.

--- ldi-nifi/ldi-nifi-processors/sparql-interactions-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/config/SparqlProcessorProperties.java
+++ ldi-nifi/ldi-nifi-processors/sparql-interactions-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/config/SparqlProcessorProperties.java
@@ -3,6 +3,7 @@ package be.vlaanderen.informatievlaanderen.ldes.ldi.processors.config;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFLanguages;
 import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.expression.ExpressionLanguageScope;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.util.StandardValidators;
 
@@ -28,7 +29,8 @@ public class SparqlProcessorProperties {
 			.displayName("SPARQL Select Query")
 			.required(true)
 			.defaultValue("SELECT ?subject ?predicate ?object WHERE {?subject ?predicate ?object}")
-			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+			.expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+			.addValidator(StandardValidators.NON_EMPTY_EL_VALIDATOR)
 			.build();
 
 	public static final PropertyDescriptor INCLUDE_ORIGINAL = new PropertyDescriptor.Builder()
